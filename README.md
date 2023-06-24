526b8f5753a88a4cc546dbb12b70a2aa4778f905
neosb <museyoucoulduse@gmail.com>
2023-06-24 02:55:56 +0200

    Ready for first clear run

--- README.md
+++ README.md
@@ -1,2 +1,161 @@
 # nutek-apple
-Nutek Security Platform for macOS operating systems
+
+Nutek Security Platform for macOS operating systems.
+
+## What it does?
+
+This - `nutek-apple.rb` - script for macOS operating systems installs
+all the neccessairly tools for well prepared hacker, bug hunter or
+a seasoned hobbiest willing to pursue a way of turning things inside out and
+is not scared to use only command line tools. Why? Almost all of the fancy
+GUI (graphical user interface) apps started as a command line tool, or are
+in many ways similar, or even less capable, because in command line - you're
+on top, you're left alone like Alice in Wonderland deep inside your dreams of power, and you know what? You can have this power! Just follow along.
+
+### List of tools
+
+* Attack
+  - bettercap
+  - ettercap
+  - hashcat
+  - hydra
+  - john-jumbo
+  - metasploit
+  - mitmproxy
+  - ncrack
+  - socat
+  - sqlmap
+  - xh
+* Code
+  - adns
+  - bvi
+  - capstone
+  - cflow
+  - cppcheck
+  - fmt
+  - gitleaks
+  - grpc
+  - hyperscan
+  - jansson
+  - radare2
+  - rats
+  - zzuf
+* Knowledge
+  - amass
+  - arp-scan
+  - bmon
+  - dnsx
+  - feroxbuster
+  - ffuf
+  - fping
+  - gau
+  - gobuster
+  - httpx
+  - httrack
+  - masscan
+  - mtr
+  - nethogs
+  - ngrep
+  - nikto
+  - nmap
+  - nuclei
+  - p0f
+  - rustscan
+  - smap
+  - tcpdump
+  - tcpflow
+  - termshark
+  - testssl
+  - zmap
+* Utility
+  - argon2
+  - bat
+  - bottom
+  - discount
+  - dos2unix
+  - dust
+  - exa
+  - expect
+  - hexyl
+  - htmlq
+  - ipcalc
+  - irssi
+  - jq
+  - jql
+  - macchina
+  - mdcat
+  - monolith
+  - neovim
+  - openvpn
+  - ouch
+  - pandoc
+  - podman
+  - podman-compose
+  - ripgrep-all
+  - sd
+  - tmux
+  - viu
+  - w3m
+  - whatmask
+
+## Installation
+
+Either clone, or download this repository to some safe location on your Apple computer. Make sure you have Ruby installed, and also Homebrew (that's from where we'll be fetching apps).
+
+Run this command:
+
+```shell
+ruby nutek-apple.rb
+```
+
+or make this happen:
+
+```shell
+chmod 744 nutek-apple.rb
+./nutek-apple.rb
+```
+
+This should present you with help message, and there you will find everything you need, but in case of clarity, I provide the transcript.
+
+```
+Usage: ruby nutek-apple.rb [options]
+Automated installation of hacking command line programs on macOS - Nutek Security Platform. Requires Homebrew.
+Curated by Nutek Security Solutions
+	and Szymon Błaszczyński.
+
+Options:
+  -h, --help				Show this help message and exit
+  -i, --install				Install programs. Choose programs to install with --attack, --code, --knowledge, --utility or --all
+  -u, --uninstall			Uninstall programs. Choose programs to uninstall with --attack, --code, --knowledge, --utility or --all
+  --all					Install or uninstall all programs
+  --attack				Install or uninstall attack programs
+  --code				Install or uninstall code programs
+  --knowledge				Install or uninstall knowledge programs
+  --utility				Install or uninstall utility programs
+  --list				List all programs
+  --list-attack				List attack programs
+  --list-code				List code programs
+  --list-knowledge			List knowledge programs
+  --list-utility			List utility programs
+  --unattended				Unattended mode. Install selected programs without asking for confirmation
+  --dry-run				Dry run. Show what would be installed without actually installing anything
+
+Examples:
+  ruby nutek-apple.rb --install --attack --utility --knowledge
+  ruby nutek-apple.rb --install --all
+  ruby nutek-apple.rb --uninstall --attack --code
+  ruby nutek-apple.rb --uninstall --all
+  ruby nutek-apple.rb --list
+  ruby nutek-apple.rb --list-attack
+  ruby nutek-apple.rb --unattended --install --attack
+  ruby nutek-apple.rb --unattended --install --all
+  ruby nutek-apple.rb --unattended --uninstall --attack
+  ruby nutek-apple.rb --unattended --uninstall --all
+
+For more information, see
+https://nutek.neosb.net/docs/tools/bing-search/ and TOOLS section where you can find more information about each tool.
+```
+
+## Why?
+
+**Nutek Security Platform** is one stop solution for your reconessaince, information gathering and establishing attack surface on targets of your choice. Have fun exploring vast library of hacking challenges, games and hackathons being like a boss. Tired of non-stop clicking mouse instead of real input to the prompt? Then this is for you. You're in control. You choose what you'll do with this tools and you are responsible for the outcome.
---
+++ nutek-apple.rb
@@ -0,0 +1,422 @@
+#!/usr/bin/env ruby
+
+$attack = [
+  "bettercap",
+  "ettercap",
+  "hashcat",
+  "hydra",
+  "john-jumbo",
+  "metasploit",
+  "mitmproxy",
+  "ncrack",
+  "socat",
+  "sqlmap",
+  "xh",
+]
+
+$code = [
+  "adns",
+  "bvi",
+  "capstone",
+  "cflow",
+  "cppcheck",
+  "fmt",
+  "gitleaks",
+  "grpc",
+  "hyperscan",
+  "jansson",
+  "radare2",
+  "rats",
+  "zzuf",
+]
+
+$knowledge = [
+  "amass",
+  "arp-scan",
+  "bmon",
+  "dnsx",
+  "feroxbuster",
+  "ffuf",
+  "fping",
+  "gau",
+  "gobuster",
+  "httpx",
+  "httrack",
+  "masscan",
+  "mtr",
+  "nethogs",
+  "ngrep",
+  "nikto",
+  "nmap",
+  "nuclei",
+  "p0f",
+  "rustscan",
+  "smap",
+  "tcpdump",
+  "tcpflow",
+  "termshark",
+  "testssl",
+  "zmap",
+]
+
+$utility = [
+  "argon2",
+  "bat",
+  "bottom",
+  "discount",
+  "dos2unix",
+  "dust",
+  "exa",
+  "expect",
+  "hexyl",
+  "htmlq",
+  "ipcalc",
+  "irssi",
+  "jq",
+  "jql",
+  "macchina",
+  "mdcat",
+  "monolith",
+  "neovim",
+  "openvpn",
+  "ouch",
+  "pandoc",
+  "podman",
+  "podman-compose",
+  "ripgrep-all",
+  "sd",
+  "tmux",
+  "viu",
+  "w3m",
+  "whatmask",
+]
+
+def load_programs(file_name)
+  programs = []
+  File.open(file_name, "r") do |file|
+      programs = file.readlines
+  end
+  return programs
+end
+
+def read_programs(programs)
+  programs.each do |program|
+      puts program
+  end
+end
+
+def install_program(program, progressbar, dry_run)
+  if dry_run
+    dry_run = "--dry-run"
+  else
+    dry_run = ""
+  end
+  if program == "metasploit"
+    system("brew tap #{dry_run} homebrew/cask") do |output|
+      print output
+    end
+    system("brew install #{dry_run} --cask #{program.chomp}") do |output|
+      print output
+    end
+    puts "✅ #{program.chomp} installed!"
+  end
+  if program == "amass"
+    system("brew tap #{dry_run} owasp-amass/amass") do |output|
+      print output
+    end
+    system("brew install #{dry_run} #{program.chomp}") do |output|
+      print output
+    end
+    puts "✅ #{program.chomp} installed!"
+  else
+    system("brew install #{dry_run} #{program.chomp}") do |output|
+      print output
+    end
+    puts "✅ #{program.chomp} installed!"
+  end
+end
+
+def uninstall_program(program, progressbar)
+  system("brew uninstall #{program.chomp}") do |output|
+      print output
+  end
+  puts "✅ #{program.chomp} uninstalled!"
+end
+
+def get_yes_no_input(prompt)
+  print prompt
+  answer = $stdin.gets
+  answer = answer.chomp.downcase
+  if answer == "yes" || answer == "y"
+    return true
+  else
+    return false
+  end
+end
+
+def get_command_line_arguments
+  args = ARGV
+  uninstall_argument = false
+  if args.length == 0 || args.include?("--help") || args.include?("-h")
+    print("\033[1;31m");
+    print("::::    ::: :::    ::: ::::::::::: :::::::::: :::    :::\n");
+    print("\033[1;31m");
+    print(":+:+:   :+: :+:    :+:     :+:     :+:        :+:   :+:\n");
+    print("\033[1;32m");
+    print(":+:+:+  +:+ +:+    +:+     +:+     +:+        +:+  +:+\n");
+    print("\033[1;32m");
+    print("+#+ +:+ +#+ +#+    +:+     +#+     +#++:++#   +#++:++ \n");
+    print("\033[1;32m");    
+    print("+#+  +#+#+# +#+    +#+     +#+     +#+        +#+  +#+\n");
+    print("\033[1;34m");
+    print("#+#   #+#+# #+#    #+#     #+#     #+#        #+#   #+#\n");
+    print("\033[1;34m");
+    print("###    ####  ########      ###     ########## ###    ###\n");
+    print("\033[0m");
+    puts "Usage: ruby nutek-apple.rb [options]"
+    puts "Automated installation of hacking command line programs on macOS - Nutek Security Platform. Requires Homebrew.\nCurated by Nutek Security Solutions\n\tand Szymon Błaszczyński."
+    puts "\nOptions:"
+    puts "  -h, --help\t\t\t\tShow this help message and exit"
+    puts "  -i, --install\t\t\t\tInstall programs. Choose programs to install with --attack, --code, --knowledge, --utility or --all"
+    puts "  -u, --uninstall\t\t\tUninstall programs. Choose programs to uninstall with --attack, --code, --knowledge, --utility or --all"
+    puts "  --web, --safari\t\t\tOpen Safari and lookup Nutek Security Platform website with all programs and tools explained"
+    puts "  --all\t\t\t\t\tInstall or uninstall all programs"
+    puts "  --attack\t\t\t\tInstall or uninstall attack programs"
+    puts "  --code\t\t\t\tInstall or uninstall code programs"
+    puts "  --knowledge\t\t\t\tInstall or uninstall knowledge programs"
+    puts "  --utility\t\t\t\tInstall or uninstall utility programs"
+    puts "  --list\t\t\t\tList all programs"
+    puts "  --list-attack\t\t\t\tList attack programs"
+    puts "  --list-code\t\t\t\tList code programs"
+    puts "  --list-knowledge\t\t\tList knowledge programs"
+    puts "  --list-utility\t\t\tList utility programs"
+    puts "  --unattended\t\t\t\tUnattended mode. Install selected programs without asking for confirmation"
+    puts "  --dry-run\t\t\t\tDry run. Show what would be installed without actually installing anything"
+    puts "\nExamples:"
+    puts "  ruby nutek-apple.rb --install --attack --utility --knowledge"
+    puts "  ruby nutek-apple.rb --install --all"
+    puts "  ruby nutek-apple.rb --uninstall --attack --code"
+    puts "  ruby nutek-apple.rb --uninstall --all"
+    puts "  ruby nutek-apple.rb --list"
+    puts "  ruby nutek-apple.rb --list-attack"
+    puts "  ruby nutek-apple.rb --unattended --install --attack"
+    puts "  ruby nutek-apple.rb --unattended --install --all"
+    puts "  ruby nutek-apple.rb --unattended --uninstall --attack"
+    puts "  ruby nutek-apple.rb --unattended --uninstall --all"
+    puts "\nFor more information, see"
+    puts "https://nutek.neosb.net/docs/tools/bing-search/ and TOOLS section where you can find more information about each tool."
+    exit
+  end
+  if args.include?("--safari") || args.include?("--web")
+    system("open -a Safari https://nutek.neosb.net/docs/tools/bing-search/")
+    exit
+  end
+  if args.include?("--list")
+    puts "Attack:"
+    read_programs($attack)
+    puts "\nCode:"
+    read_programs($code)
+    puts "\nKnowledge:"
+    read_programs($knowledge)
+    puts "\nUtility:"
+    read_programs($utility)
+    puts "\nFor more information, see"
+    puts "https://nutek.neosb.net/docs/tools/bing-search/ and TOOLS section where you can find more information about each tool."
+    exit
+  end
+  if args.include?("--list-attack")
+    puts "Attack:"
+    read_programs($attack)
+    puts "\nFor more information, see"
+    puts "https://nutek.neosb.net/docs/tools/bing-search/ and TOOLS section where you can find more information about each tool."
+    exit
+  end
+  if args.include?("--list-code")
+    puts "Code:"
+    read_programs($code)
+    puts "\nFor more information, see"
+    puts "https://nutek.neosb.net/docs/tools/bing-search/ and TOOLS section where you can find more information about each tool."
+    exit
+  end
+  if args.include?("--list-knowledge")
+    puts "Knowledge:"
+    read_programs($knowledge)
+    puts "\nFor more information, see"
+    puts "https://nutek.neosb.net/docs/tools/bing-search/ and TOOLS section where you can find more information about each tool."
+    exit
+  end
+  if args.include?("--list-utility")
+    puts "Utility:"
+    read_programs($utility)
+    puts "\nFor more information, see"
+    puts "https://nutek.neosb.net/docs/tools/bing-search/ and TOOLS section where you can find more information about each tool."
+    exit
+  end
+  if args.include?("--uninstall") || args.include?("-u")
+    uninstall_argument = true
+  end
+  if args.include?("--install") || args.include?("-i")
+    uninstall_argument = false
+  end
+  if args.include?("--uninstall") && args.include?("--install")
+    puts "❌ Error: --uninstall and --install cannot be used together"
+    exit false
+  end
+  if args.include?("-u") && args.include?("-i")
+    puts "❌ Error: -u and -i cannot be used together"
+    exit false
+  end
+  if args.include?("--uninstall") && args.include?("-i")
+    puts "❌ Error: --uninstall and -i cannot be used together"
+    exit false
+  end
+  if args.include?("-u") && args.include?("--install")
+    puts "❌ Error: -u and --install cannot be used together"
+    exit false
+  end
+  if args.include?("--uninstall") && args.include?("-u")
+    puts "❌ Error: --uninstall and -u cannot be used together"
+    exit false
+  end
+  if args.include?("-i") && args.include?("--install")
+    puts "❌ Error: -i and --install cannot be used together"
+    exit false
+  end
+  if args.include?("-u") && args.include?("--uninstall")
+    puts "❌ Error: -u and --uninstall cannot be used together"
+    exit false
+  end
+  if ( args.include?("-u") || args.include?("--uninstall") ) &&
+    args.include?("--dry-run")
+    puts "❌ Error: -u and --uninstall cannot be used with --dry-run"
+    exit false
+  end
+  if (args.include?("--install") || args.include?("-i")) &&
+    ( !args.include?("--all") && !args.include?("--attack") && 
+    !args.include?("--utility") && !args.include?("--code") && 
+    !args.include?("--knowledge") )
+    puts "❌ Error: -i and --install must be used with --all, --attack, --utility, --code, or --knowledge"
+    exit false
+  end
+  if (args.include?("--uninstall") || args.include?("-u")) &&
+    ( !args.include?("--all") && !args.include?("--attack") &&
+    !args.include?("--utility") && !args.include?("--code") &&
+    !args.include?("--knowledge") )
+    puts "❌ Error: -u and --uninstall must be used with --all, --attack, --utility, --code, or --knowledge"
+    exit false
+  end
+  programs = []
+  if args.include?("--attack")
+    programs += $attack
+  end
+  if args.include?("--utility")
+    programs += $utility
+  end
+  if args.include?("--code")
+    programs += $code
+  end
+  if args.include?("--knowledge")
+    programs += $knowledge
+  end
+  if args.include?("--all")
+    programs = $attack + $utility + $code + $knowledge
+  end
+  if args.include?("--unattended")
+    unattended = true
+  else
+    unattended = false
+  end
+  if args.include?("--dry-run")
+    dry_run = true
+  else
+    dry_run = false
+  end
+  if programs.empty?
+    puts "❌ Error: No programs selected. Please select at least one program group to install or uninstall."
+    exit false
+  end
+  return uninstall_argument, programs, unattended, dry_run
+end
+
+def check_if_homebrew_installed
+  command = "which brew"
+  output = `#{command}`
+  if output.empty?
+    puts "❌ Homebrew is not installed. Please install Homebrew and try again. Visit https://brew.sh/ for more information."
+    exit false
+  else
+    puts "✅ Homebrew is installed."
+  end
+end
+
+def main
+  uninstall_argument, programs, 
+  unattended, dry_run = get_command_line_arguments
+  check_if_homebrew_installed
+  # progressbar = ProgressBar.new(78)
+  progressbar = nil
+  if uninstall_argument
+    answer = false
+    if !unattended
+      puts programs
+      answer = get_yes_no_input "Are you sure you want to uninstall all the above programs? (yes/no) "
+    else
+      answer = true
+    end
+    if answer
+      programs.each do |program|
+        puts "Uninstalling #{program}..."
+        uninstall_program(program, progressbar)
+        sleep(0.1)
+      end
+    else
+      puts "Uninstall aborted!"
+    end
+  else
+    answer = false
+    if !unattended
+      puts programs
+      answer = get_yes_no_input "Are you sure you want to install all the above programs? (yes/no) "
+    else
+      answer = true
+    end
+    if answer
+      programs.each do |program|
+        puts "Installing #{program}..."
+        install_program(program, progressbar, dry_run)
+        sleep(0.1)
+      end
+      puts "\nFor more information, see"
+      puts "https://nutek.neosb.net/docs/tools/bing-search/ and TOOLS section where you can find more information about each tool."
+    else
+      puts "Install aborted!"
+    end
+  end
+end
+
+main
+
+####################
+# End of Script
+####################
+
+####################
+# Notes
+####################
+# This script is designed to be run on macOS.
+# This script is tested to run with Ruby 3.2.2 and Homebrew 4.0.24
+
+####################
+# License
+####################
+# MIT License
+#
+# This script is part of the Nutek Security Platform (https://nutek.neosb.net/) provided by Nutek Security Solutions and Szymon Błaszczyński.
+# You can use this script for free. Please read the license agreement below.
+# If you do not agree to this license, you cannot use this script.
+#
+# MIT License
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this script and associated documentation files (the "Script"), to deal in the Script without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Script, and to permit persons to whom the Script is furnished to do so, subject to the following conditions:
+# The above license notice and this permission notice shall be included in all copies or substantial portions of the Script.
+# THE SCRIPT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SCRIPT OR THE USE OR OTHER DEALINGS IN THE SCRIPT.
+####################
